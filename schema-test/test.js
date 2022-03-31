7c993669757957f2b5f7a6173e5b66b196fce143
卞刘柱 <ext.bianliuzhu@jd.com>
2022-03-31 14:06:22 +0800

    feat: ajv 学习

--- schema-test/test.js
+++ schema-test/test.js
@@ -1,22 +1,71 @@
 const Ajv = require("ajv");
-const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+const addFormats = require("ajv-formats");
+const localize = require("ajv-i18n");
+const ajv = new Ajv(); // options can be passed, e.g. {}
 
+addFormats(ajv);
 const schema = {
   type: "object",
+  additionalProperties: false, // 额外属性
+  required: ["name", "age"],
   properties: {
-    foo: { type: "integer" },
-    bar: { type: "string" },
+    name: {
+      type: "string",
+      minLength: 3,
+      maxLength: 6,
+    },
+    age: {
+      type: "number",
+    },
+    pets: {
+      type: "array",
+    },
+    isWorks: {
+      type: "boolean",
+    },
+    email: {
+      type: "string",
+      format: "email",
+    },
+    Gleason: {
+      range: true,
+    },
   },
-  required: ["foo"],
-  additionalProperties: false,
 };
+// ajv.addFormat("test", (data) => {
+//   console.log("------->", data);
+//   return data === "haha";
+// });
 
+/// function 方式
+// ajv.addKeyword("range", {
+//   validate(schema, data) {
+//     if (Array.isArray(data)) return schema;
+//     return false;
+//   },
+// });
+
+ajv.addKeyword("range", {
+  compile(schema, parentSchema) {
+    console.log(schema, parentSchema);
+    return () => true;
+  },
+});
 const validate = ajv.compile(schema);
 
 const data = {
-  foo: 1,
-  bar: "abc",
+  name: "12341",
+  age: "23243",
+  isWorks: true,
+  pets: ["dog", 123],
+  email: "bianliuzhu@gmail.com",
+  Gleason: [1, 1],
 };
 
 const valid = validate(data);
-if (!valid) console.log(validate.errors);
+if (valid) {
+  console.log("验证通过");
+} else {
+  localize.zh(validate.errors);
+  console.log(validate.errors);
+}
