14e7b58647d97907ce8217eb5e9a2fdea58b05ac
卞刘柱 <ext.bianliuzhu@jd.com>
2022-04-06 18:30:06 +0800

    feat: 配置ts

--- lib/schema-from.tsx
+++ lib/schema-from.tsx
@@ -1,6 +1,12 @@
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
+import { Schema } from "./types";
 
 export default defineComponent({
+	props: {
+		schema: {
+			type: Object as PropType<Schema>,
+		},
+	},
 	name: "SchemaForm",
 	setup(props, { slots, emit, attrs }) {
 		return () => {
