bae1c51fa038446a0d5c1d91ecfbeeeb7b8841f8
Gleason <bianliuzhu@gmail.com>
2022-04-24 14:14:31 +0800

    feat: 添加功能

---
+++ lib/fields/string-field.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-04-08 13:48:02
+ * @LastEditTime: 2022-04-08 18:05:40
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vite-vue-ts\lib\fields\string-field.vue
+-->
+<template>
+	<input type="text" />
+</template>
+
+<script setup lang="ts"></script>
